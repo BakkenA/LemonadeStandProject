cae244342e5c1174f46b62533e9200b86237ddd1
Adam Bakken <Adam Bakken>
2016-10-24 11:50:02 -0500

    Day 1 establishment of classes and game launcher

---
+++ PotionShop/PotionShop.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotionShop
+{
+    class PotionShop
+    {
+        public PotionShop()
+        {
+        }
+
+        public void LaunchGame()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Welcome to Potion Shop!\nPlease choose one of the following:\nNEW GAME\nTUTORIAL");
+                string menuChoice = Console.ReadLine().ToUpper();
+                if (menuChoice == "NEW GAME")
+                {
+                    CreateGame();
+                }
+                else if (menuChoice == "TUTORIAL")
+                {
+                    RunTutorial();
+                }
+                else if (menuChoice == "666")
+                {
+                    Console.Clear();
+                    RunHardMode();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice, please choose again.");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.Clear();
+                }
+            }
+        }
+        public void CreateGame()
+        {
+
+        }
+        public void RunTutorial()
+        {
+
+        }
+        public void RunHardMode()
+        {
+            if(Console.BackgroundColor == ConsoleColor.Black)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Console.WriteLine("HARD MODE ENGAGED!!!");
+                LaunchGame();
+            }
+            else if (Console.BackgroundColor == ConsoleColor.Red)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Clear();
+                Console.WriteLine("HARD MODE DISENGAGED!!!");
+                LaunchGame();
+            }
+        }
+    }
+}
---
+++ PotionShop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotionShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PotionShop PotionVendor = new PotionShop();
+            PotionVendor.LaunchGame();
+        }
+    }
+}
